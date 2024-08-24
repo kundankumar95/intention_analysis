a94410ba6a477e3d3d456b88151ce248f5d0ddbc
kundan kumar <129506620+kundankumar95@users.noreply.github.com>
2024-08-24 07:24:41 +0530

    Update data_preprocessing.py

--- intention_analysis/data_preprocessing.py
+++ intention_analysis/data_preprocessing.py
@@ -3,17 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 def preprocess_data(df):
-    # Downloading NLTK data
     nltk.download('punkt')
 
-    # Tokenization
     df['tokens'] = df['text'].apply(nltk.word_tokenize)
 
-    # Encode the labels (intentions)
     le = LabelEncoder()
     df['label'] = le.fit_transform(df['intention'])
 
-    # Split data into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], test_size=0.2, random_state=42)
 
     return X_train, X_test, y_train, y_test, le
